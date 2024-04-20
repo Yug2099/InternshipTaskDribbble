bc1dab4dc540645e09bd8afd569b2370e77ef106
Yug2099 <yug.g03@gmail.com>
2024-04-20 13:16:43 +0530

    Local Storage

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -64,9 +64,9 @@ const Homepage = () => {
         { ...formData },
         config
       );
-      localStorage.setItem("userInfo", response.data);
-      console.log("response.data: ",response.data);
-      console.log("response: ",response)
+      localStorage.setItem("userInfo", JSON.stringify(response.data));
+      console.log("response.data: ", response.data);
+      console.log("response: ", response);
       if (response.data) {
         // User successfully registered
         navigate(`/setprofile/${response.data.userId}`);
